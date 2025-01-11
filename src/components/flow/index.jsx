336b788aa8c5a5a890d87e905867b2e76b123705
tinchomengo <tinchomengo@gmail.com>
2025-01-10 21:39:07 -0300

    fix: uppercase typo

--- src/components/flow/index.jsx
+++ src/components/flow/index.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import TitlePage from '../titlePage'
-import "./flow.css"
+import React from "react";
+import TitlePage from "../titlePage";
+import "./flow.css";
 
 function Flow() {
   return (
-    <div className='page_container'>
-        <TitlePage title="Flow" subtitle="Integrations are effortless; adaptation is seamless. We streamline complex data into a fluid narrative that evolves as you do."/>
-        <img src="static/images/vector.svg" alt="" className='flow_container' />
-
+    <div className="page_container">
+      <TitlePage
+        title="Flow"
+        subtitle="Integrations are effortless; adaptation is seamless. We streamline complex data into a fluid narrative that evolves as you do."
+      />
+      <img src="static/images/Vector.svg" alt="" className="flow_container" />
     </div>
-  )
+  );
 }
 
-export default Flow
+export default Flow;
