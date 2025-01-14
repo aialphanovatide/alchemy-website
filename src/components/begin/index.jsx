52337867416fc81cd82d56f372333c4690bf6bf2
tinchomengo <tinchomengo@gmail.com>
2025-01-14 19:16:53 -0300

    finished tab 6 for desktop and mobile

--- src/components/begin/index.jsx
+++ src/components/begin/index.jsx
@@ -1,16 +1,42 @@
-import React from 'react'
-import './begin.css'
+import React, { useRef } from "react";
+import "./begin.css";
 
 function Begin() {
-  return (
-    <div className='page_container'>
-        <h2 className='title_begin'>Begin</h2>
-        <p className='subtitle_begin'>Ready to think beyond the ordinary?</p>
-        <div className='begin_container'>
+  const videoRef = useRef(null);
 
+  // When the video ends, jump to second 5 and play again.
+  const handleVideoEnd = () => {
+    if (videoRef.current) {
+      videoRef.current.currentTime = 5;
+      videoRef.current.play();
+    }
+  };
+
+  return (
+    <div className="page_container">
+      <div className="begin_video_wrapper">
+        <video
+          ref={videoRef}
+          src="/static/videos/contactCloud.mp4"
+          autoPlay
+          muted
+          className="begin_background_video"
+          onEnded={handleVideoEnd}
+        />
+
+        <div className="video_button_container">
+          <a href="" className="demo_button">
+            Book a Demo
+          </a>
         </div>
+      </div>
+
+      <h2 className="title_begin">Begin</h2>
+      <p className="subtitle_begin">Ready to think beyond the ordinary?</p>
+
+      <div className="begin_container"></div>
     </div>
-  )
+  );
 }
 
-export default Begin
+export default Begin;
