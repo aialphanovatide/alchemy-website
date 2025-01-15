2a1c1569974a8277d326851929a8504c811d825b
tinchomengo <tinchomengo@gmail.com>
2025-01-14 23:31:47 -0300

    test for corrections

--- src/components/tools/index.jsx
+++ src/components/tools/index.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
-import "./tools.css"
-import { useEffect, useRef } from 'react'
-import TitlePage from '../titlePage'
-function Tools({isActive}) {
-    const gifRef = useRef(null);
+import React from "react";
+import "./tools.css";
+import { useEffect, useRef } from "react";
+import TitlePage from "../titlePage";
+function Tools({ isActive }) {
+  const gifRef = useRef(null);
 
-    useEffect(() => {
-        if (gifRef.current) {
-          gifRef.current.src = isActive ? "static/images/Tools Animation.gif" : "";
-        }
-      }, [isActive]);
+  useEffect(() => {
+    if (gifRef.current) {
+      gifRef.current.src = isActive ? "static/images/toolsAnimation.gif" : "";
+    }
+  }, [isActive]);
   return (
-    <div className='page_container'>
-        <TitlePage title="Tools" subtitle="Our AI Agents orchestrate every aspect of your marketing, from analytics to action—so you’re not just reacting, you’re shaping outcomes."/>
-        <img ref={gifRef} src="static/images/Tools Animation.gif" alt=""  />
-        <p className={`first_begin ${isActive ? 'active' : ''}`}>A dynamic dashboard powered by a swarm of intelligent agents.</p>
-        <p className={`second_begin ${isActive ? 'active' : ''}`}> Track key metrics in real time, then act on them automatically.</p>
-        <p className={`third_begin ${isActive ? 'active' : ''}`}>Move beyond static reports: let the system suggest—and implement—strategic moves.</p>
+    <div className="page_container">
+      <TitlePage
+        title="Tools"
+        subtitle="Our AI Agents orchestrate every aspect of your marketing, from analytics to action—so you’re not just reacting, you’re shaping outcomes."
+      />
+      <img ref={gifRef} src="static/images/toolsAnimation.gif" alt="" />
+      <p className={`first_begin ${isActive ? "active" : ""}`}>
+        A dynamic dashboard powered by a swarm of intelligent agents.
+      </p>
+      <p className={`second_begin ${isActive ? "active" : ""}`}>
+        {" "}
+        Track key metrics in real time, then act on them automatically.
+      </p>
+      <p className={`third_begin ${isActive ? "active" : ""}`}>
+        Move beyond static reports: let the system suggest—and
+        implement—strategic moves.
+      </p>
     </div>
-  )
+  );
 }
 
-export default Tools
+export default Tools;
